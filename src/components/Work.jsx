5ac50e904c77f4353419f74b16ed9e22b4ce2d4d
auma2022 <auma.amwayi@student.moringaschool.com>
2022-07-28 20:12:01 +0300

    add the projects done

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,8 +1,169 @@
 import React from 'react'
+import Facebook from '../assets/facebookclone.jpeg';
+import Medium from '../assets/medium.jpeg';
+import Tiktok from '../assets/tiktok.jpeg';
+import Instagram from '../assets/insta.jpeg';
+import Twitter from '../assets/twitter.jpeg';
+import Netflix from '../assets/netflix.jpeg';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-[#9daaf2] bg-[#1a2238]'>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 text-[#ff6a3d] border-[#f4db7d]'>
+            Successfull projects
+          </p>
+          <p className='py-6'>Check out some of my recent working projects</p>
+        </div>
+
+{/* Container */}
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Facebook})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-[#9daaf2] tracking-wider'>
+                Facebook Clone Using React JS
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Medium})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-[#9daaf2] tracking-wider'>
+                Medium 3.0 Clone
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Instagram})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-[#9daaf2] tracking-wider'>
+                Instagram clone
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Tiktok})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-[] tracking-wider'>
+                Tiktok clone
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+            {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Twitter})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-[#9daaf2] tracking-wider'>
+                Twitter clone
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Netflix})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-[#9daaf2] tracking-wider'>
+                Netflix clone
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-[#ff6a3d] text-[#1a2238] font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
